3c89909a5a64c8a7eaf35d6ee07c39f2ddb6709c
Simon Weller <s.meierbeck@gmail.com>
2020-06-13 13:46:21 +0200

    enter edit mode on double click

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,20 +1,27 @@
 <script>
+  import { afterUpdate } from "svelte";
+
   export let id;
   export let text;
   export let completed;
   export let toggleTodo;
   export let deleteTodo;
+
+  let editing = false;
+  let input;
+
+  afterUpdate(() => editing && input && input.focus());
 </script>
 
-<li class:completed>
+<li class:completed class:editing>
   <div class="view">
     <input
       class="toggle"
       type="checkbox"
       checked={completed}
       on:change={() => toggleTodo(id)} />
-    <label>{text}</label>
-    <button class="destroy" on:click={() => deleteTodo(id)}/>
+    <label on:dblclick={() => (editing = !editing)}>{text}</label>
+    <button class="destroy" on:click={() => deleteTodo(id)} />
   </div>
-  <input class="edit" value={text} />
+  <input class="edit" value={text} bind:this={input} />
 </li>
