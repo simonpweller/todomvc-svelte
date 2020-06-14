1c4482e481623f34974a3555bfa42fcdc9634afd
Simon Weller <s.meierbeck@gmail.com>
2020-06-14 12:35:18 +0200

    add routes / filters

--- src/App.svelte
+++ src/App.svelte
@@ -1,72 +1,10 @@
 <script>
-  import { v4 as uuid } from "uuid";
-  import Todo from "./Todo.svelte";
-  import Header from "./Header.svelte";
-  import TodoCount from "./TodoCount.svelte";
-  import ClearCompletedButton from "./ClearCompletedButton.svelte";
+  import Router from "./Router.svelte";
+  import TodoList from "./TodoList.svelte";
   import InfoFooter from "./InfoFooter.svelte";
-
-  let todos = JSON.parse(localStorage.getItem("todos")) || [];
-  $: localStorage.setItem("todos", JSON.stringify(todos));
-
-  $: todoCount = todos.length;
-  $: activeTodoCount = todos.filter((todo) => !todo.completed).length;
-
-  $: hasTodos = todoCount > 0;
-  $: hasCompleted = hasTodos && activeTodoCount < todoCount;
-  $: allCompleted = activeTodoCount === 0;
-
-  function addTodo(newTodoText) {
-    todos = [...todos, { id: uuid(), text: newTodoText, completed: false }];
-  }
-
-  function deleteTodo(id) {
-    todos = todos.filter((todo) => todo.id !== id);
-  }
-
-  function deleteCompleted() {
-    todos = todos.filter((todo) => !todo.completed);
-  }
-
-  function toggleAll() {
-    todos = todos.map((todo) => ({ ...todo, completed: !allCompleted }));
-  }
 </script>
 
-<section class="todoapp">
-  <Header {addTodo} />
-  {#if hasTodos}
-    <section class="main">
-      <input
-        id="toggle-all"
-        class="toggle-all"
-        type="checkbox"
-        on:click={toggleAll}
-        checked={allCompleted} />
-      <label for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        {#each todos as todo (todo.id)}
-          <Todo bind:todo {deleteTodo} />
-        {/each}
-      </ul>
-    </section>
-    <footer class="footer">
-      <TodoCount count={activeTodoCount} />
-      <ul class="filters">
-        <li>
-          <a class="selected" href="#/">All</a>
-        </li>
-        <li>
-          <a href="#/active">Active</a>
-        </li>
-        <li>
-          <a href="#/completed">Completed</a>
-        </li>
-      </ul>
-      {#if hasCompleted}
-        <ClearCompletedButton {deleteCompleted} />
-      {/if}
-    </footer>
-  {/if}
-</section>
+<Router let:filter>
+  <TodoList {filter} />
+</Router>
 <InfoFooter />
---
+++ src/Router.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router } from "director/build/director";
+  let filter = null;
+  const router = new Router({
+    "/active": () => (filter = "active"),
+    "/completed": () => (filter = "completed"),
+    "/": () => (filter = null),
+  });
+  router.init();
+</script>
+
+<slot {filter} />
