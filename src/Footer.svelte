90aa4e59d560318087e159e5e190bd4613f5ded9
Simon Weller <s.meierbeck@gmail.com>
2020-06-20 23:20:30 +0200

    Extract Filters from Footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,27 +1,14 @@
 <script>
   import TodoCount from './TodoCount.svelte';
+  import Filters from './Filters.svelte';
   import ClearCompletedButton from './ClearCompletedButton.svelte';
-  import { filter, hasTodos, hasCompleted, activeTodoCount } from './stores';
+  import { hasTodos, hasCompleted, activeTodoCount } from './stores';
 </script>
 
 {#if $hasTodos}
   <footer class="footer">
     <TodoCount count={$activeTodoCount} />
-    <ul class="filters">
-      <li>
-        <a class:selected={!$filter} href="#/">All</a>
-      </li>
-      <li>
-        <a class:selected={$filter === 'active'} on:keypress href="#/active">
-          Active
-        </a>
-      </li>
-      <li>
-        <a class:selected={$filter === 'completed'} href="#/completed">
-          Completed
-        </a>
-      </li>
-    </ul>
+    <Filters />
     {#if $hasCompleted}
       <ClearCompletedButton />
     {/if}
