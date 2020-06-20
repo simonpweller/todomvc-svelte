09d6c4a05e1d297225f705f5741c07ceb3d812fb
Simon Weller <s.meierbeck@gmail.com>
2020-06-20 23:14:11 +0200

    Inline TodoList

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
   import { Router } from 'director/build/director';
-  import TodoList from './TodoList.svelte';
   import { filter } from './stores.js';
+  import Header from './Header.svelte';
+  import Main from './Main.svelte';
+  import Footer from './Footer.svelte';
+  import InfoFooter from './InfoFooter.svelte';
 
   const router = new Router({
     '/active': () => ($filter = 'active'),
@@ -11,4 +14,9 @@
   router.init();
 </script>
 
-<TodoList />
+<section class="todoapp">
+  <Header />
+  <Main />
+  <Footer />
+</section>
+<InfoFooter />
