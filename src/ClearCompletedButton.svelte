dd6ffd502e5b28dfe22dd627222ba51f3879e878
Simon Weller <s.meierbeck@gmail.com>
2020-06-20 23:22:19 +0200

    Shift if hasCompleted check into ClearCompletedButton

--- src/ClearCompletedButton.svelte
+++ src/ClearCompletedButton.svelte
@@ -1,7 +1,9 @@
 <script>
-  import { todos } from "./stores";
+  import { todos, hasCompleted } from './stores';
 </script>
 
-<button class="clear-completed" on:click={todos.deleteCompleted}>
-  Clear completed
-</button>
+{#if $hasCompleted}
+  <button class="clear-completed" on:click={todos.deleteCompleted}>
+    Clear completed
+  </button>
+{/if}
--- src/Footer.svelte
+++ src/Footer.svelte
@@ -2,15 +2,13 @@
   import TodoCount from './TodoCount.svelte';
   import Filters from './Filters.svelte';
   import ClearCompletedButton from './ClearCompletedButton.svelte';
-  import { hasTodos, hasCompleted, activeTodoCount } from './stores';
+  import { hasTodos, activeTodoCount } from './stores';
 </script>
 
 {#if $hasTodos}
   <footer class="footer">
     <TodoCount count={$activeTodoCount} />
     <Filters />
-    {#if $hasCompleted}
-      <ClearCompletedButton />
-    {/if}
+    <ClearCompletedButton />
   </footer>
 {/if}
